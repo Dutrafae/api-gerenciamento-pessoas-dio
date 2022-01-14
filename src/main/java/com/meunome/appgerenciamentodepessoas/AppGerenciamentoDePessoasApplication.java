0f3fc7972a07c2e65bcf3c0fe14fe203c2944475
dutrafae <dutrafae@gmail.com>
2022-01-14 18:03:05 -0300

    first commit

---
+++ src/main/java/com/meunome/appgerenciamentodepessoas/AppGerenciamentoDePessoasApplication.java
@@ -0,0 +1,13 @@
+package com.meunome.appgerenciamentodepessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppGerenciamentoDePessoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppGerenciamentoDePessoasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meunome/appgerenciamentodepessoas/AppGerenciamentoDePessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meunome.appgerenciamentodepessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppGerenciamentoDePessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
